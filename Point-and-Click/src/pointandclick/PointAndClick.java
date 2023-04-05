9c9f7f284d9494060eefc1a879caaef9845dd98d
Eric Anderson <ande28em@dukes.jmu.edu>
2023-04-05 15:01:40 -0400

    Initial stubs and folder layout

---
+++ Point-and-Click/src/pointandclick/PointAndClick.java
@@ -0,0 +1,24 @@
+package pointandclick;
+
+/**
+ * PointAndClick.java - Main class to start the CS349 point and click game.
+ * 
+ * This work complies with the JMU Honor Code. 
+ * 
+ * @author Eric Anderson
+ * @version Apr 5, 2023
+ */
+public class PointAndClick
+{
+
+  /**
+   *
+   * @param args
+   */
+  public static void main(String[] args)
+  {
+    // TODO Auto-generated method stub
+
+  }
+
+}
