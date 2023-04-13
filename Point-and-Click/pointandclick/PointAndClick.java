8c6dfbcb8cb7efe1880e270e3b4eca0fe17fbf4f
Andrew Davis <davis7aj@dukes.jmu.edu>
2023-04-13 11:25:14 -0400

    Added start and option buttons.

--- Point-and-Click/pointandclick/PointAndClick.java
+++ Point-and-Click/pointandclick/PointAndClick.java
@@ -1,15 +1,22 @@
 package pointandclick;
 
 import java.awt.Color;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 
+import javax.imageio.ImageIO;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -19,6 +26,8 @@ import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+
+import io.ResourceFinder;
 
 /**
  * PointAndClick.java - Main class to start the CS349 point and click game.
@@ -33,7 +42,21 @@ public class PointAndClick implements Runnable, ActionListener
   protected static final String ABOUT = "About";
   static final String EXIT = "Exit";
 
+
+  private static String HELP = "Help";
+  private static String START = "Start";
+  private static String BUTTONA = "buttonA";
+  private static String BUTTONB = "buttonB";
+  private static String BUTTONC = "buttonC";
+  private static String BUTTOND = "buttonD";
+  private JButton startButton;
+  private JButton buttonA;
+  private JButton buttonB;
+  private JButton buttonC;
+  private JButton buttonD;
+
   JFrame frame;
+  JPanel contentPane;
 
   /**
    * @param args
@@ -53,7 +76,7 @@ public class PointAndClick implements Runnable, ActionListener
     this.frame = new JFrame("The Final Adventure");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    JPanel contentPane = (JPanel) frame.getContentPane();
+    contentPane = (JPanel) frame.getContentPane();
 
     contentPane.setLayout(null);
 
@@ -81,15 +104,68 @@ public class PointAndClick implements Runnable, ActionListener
 
     exit.setMnemonic(KeyEvent.VK_Q); // menu must first be open
 
+    JMenu help = new JMenu(HELP);
+    menuBar.add(help);
+    JMenuItem about = new JMenuItem(ABOUT);
+    about.addActionListener(this);
+    help.add(about);
+
+    // ImageIcon icon = new ImageIcon("pointandclick/resources/Nature_Distraction.jpg");
+
+    ResourceFinder rf = ResourceFinder.createInstance(new resources.Marker());
+
+    BufferedImage bg;
+    Image bg2;
+
+    try
+    {
+      InputStream is = rf.findInputStream("ISAT_Entering.jpg");
+      bg = ImageIO.read(is);
+      bg2 = bg.getScaledInstance(720, 780, Image.SCALE_SMOOTH);
+      JLabel backGround = new JLabel(new ImageIcon(bg2));
+      backGround.setBounds(0, 0, 720, 780);
+      frame.add(backGround);
+    }
+    catch (IOException e)
+    {
+
+    }
+
+    startButton = new JButton("Start");
+    startButton.setBounds(275, 600, 150, 50);
+    startButton.addActionListener(this);
+    contentPane.add(startButton);
+
+    buttonA = new JButton("First Option A");
+    buttonA.setActionCommand("buttonA");
+    buttonA.setBounds(200, 600, 150, 50);
+    buttonA.addActionListener(this);
+    contentPane.add(buttonA);
+    buttonA.setVisible(false);
+
+    buttonB = new JButton("First Option B");
+    buttonB.setActionCommand("buttonB");
+    buttonB.setBounds(360, 600, 150, 50);
+    buttonB.addActionListener(this);
+    contentPane.add(buttonB);
+    buttonB.setVisible(false);
+
+    buttonC = new JButton("First Option C");
+    buttonC.setActionCommand("buttonC");
+    buttonC.setBounds(200, 655, 150, 50);
+    buttonC.addActionListener(this);
+    contentPane.add(buttonC);
+    buttonC.setVisible(false);
+
+    buttonD = new JButton("First Option D");
+    buttonD.setActionCommand("buttonD");
+    buttonD.setBounds(360, 655, 150, 50);
+    buttonD.addActionListener(this);
+    contentPane.add(buttonD);
+    buttonD.setVisible(false);
+
     frame.setSize(720, 780);
 
-    Icon icon = new ImageIcon("InfoIcon.png");
-
-    JButton button1 = new JButton(icon);
-    button1.setBounds(70, 5, 100, 100);
-    button1.setActionCommand(ABOUT);
-    button1.addActionListener(this);
-    contentPane.add(button1);
 
     contentPane.setBackground(Color.GRAY);
     frame.setBackground(Color.GRAY);
@@ -111,8 +187,6 @@ public class PointAndClick implements Runnable, ActionListener
     String actionCommand;
     actionCommand = evt.getActionCommand();
 
-    // code I wrote for another of my projects; to be edited to fit this one later
-
     if (actionCommand.equals(EXIT))
     {
 
@@ -131,6 +205,56 @@ public class PointAndClick implements Runnable, ActionListener
 
       return;
     }
+
+
+    // can only be pressed once per iteration
+    if (actionCommand.equals(START))
+    {
+      startButton.setVisible(false);
+
+      buttonA.setVisible(true);
+      buttonB.setVisible(true);
+      buttonC.setVisible(true);
+      buttonD.setVisible(true);
+
+    }
+
+    if (actionCommand.equals(BUTTONA))
+    {
+
+      buttonA.setText("next option A");
+      buttonB.setText("next option B");
+      buttonC.setText("next option C");
+      buttonD.setText("next option D");
+    }
+
+    if (actionCommand.equals(BUTTONB))
+    {
+
+      buttonA.setText("next option A");
+      buttonB.setText("next option B");
+      buttonC.setText("next option C");
+      buttonD.setText("next option D");
+    }
+
+    if (actionCommand.equals(BUTTONC))
+    {
+
+      buttonA.setText("next option A");
+      buttonB.setText("next option B");
+      buttonC.setText("next option C");
+      buttonD.setText("next option D");
+    }
+
+    if (actionCommand.equals(BUTTOND))
+    {
+
+      buttonA.setText("next option A");
+      buttonB.setText("next option B");
+      buttonC.setText("next option C");
+      buttonD.setText("next option D");
+    }
+
 
   }
 
